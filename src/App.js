78b019560002d6816db9f98ca097fedd74c77d1d
Zvi Schvarcz <zvischvarcz@gmail.com>
2023-07-25 12:04:04 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import { evaluate } from 'mathjs';
 import './App.css';
 
 function App() {
+  const [display, setDisplay] = useState()
+  const buttonContents = [ "1", "2", "3", " + ", "4", "5", "6", " - ", "7", "8", "9", " * ", "C", "0", "=", " / "]
+  const sumFunc = (char) => {
+    if (display === undefined){
+      setDisplay(char);
+    }else if (char !== "=" && char !== "C"){
+      setDisplay(display + char);
+    }else if(char === "="){
+      setDisplay(evaluate(display));
+    } else {
+      setDisplay("")
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='calcWrap'>
+      <div className='textWrap'>
+        <h1 className='text'>{display}</h1>
+      </div>
+      <div className='buttonWrap'>
+        {buttonContents.map((char, index) => {
+          return (
+            <Buttons key={index} click={() => sumFunc(char)} char={char}/>
+          )
+        })}
+      </div>
     </div>
   );
 }
+
+const Buttons = (props) => {
+  return (
+    <div>
+      <button className='buttons' onClick={props.click}>{props.char}</button>
+    </div>
+  )
+}
+
+
 
 export default App;
