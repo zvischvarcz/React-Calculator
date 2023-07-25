9340b05b5ece7dbcc4f19620c5bc0fc7172acc7d
Zvi Schvarcz <zvischvarcz@gmail.com>
2023-07-25 16:29:09 +0100

    Added keypress

--- src/App.js
+++ src/App.js
@@ -1,33 +1,56 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { evaluate } from 'mathjs';
 import './App.css';
 
 function App() {
-  const [equalPressed, setEqualPressed] = useState( false)
-  const [display, setDisplay] = useState()
-  const [answer, setAnswer] = useState()
-  const buttonContents = [ "1", "2", "3", "C", "4", "5", "6", " * ", "7", "8", "9", " / ", " + ", "0", " - ", "=", "(", ")", "sqrt", "Ans"]
+  const [equalPressed, setEqualPressed] = useState( false);
+  const [display, setDisplay] = useState("");
+  const [answer, setAnswer] = useState("");
+  // const [currentAnswer, setCurrentAnswer] = useState();
+  const buttonContents = [ "1", "2", "3", "C", "4", "5", "6", "*", "7", "8", "9", "/", "+", "0", "-", "=", "(", ")", "sqrt", "Ans"];
+  
+
+
+  // useEffect(() => {
+    document.addEventListener("keypress", (event) => {
+      for(let i = 0; i < buttonContents.length; i++){
+        if (event.key === buttonContents[i]) 
+          sumFunc(event.key);
+      }
+    })
+  // }, [])
+  
+
   const sumFunc = (char) => {
+    
     if(char === "C"){
       setDisplay("");
     } else if (char === "Ans"){
       setDisplay(answer);
       setEqualPressed(false);
-    }else if (display === undefined || equalPressed === true){
-      setDisplay(char);
+    }else if (display === "" || equalPressed === true){
+      setDisplay(display + char);
+      console.log(display, char)
       setEqualPressed(false);
     }else if (char !== "=" && char !== "C" && char !== "Ans"){
       setDisplay(display + char);
+      
     }else if(char === "="){
       setEqualPressed(true);
       setAnswer(evaluate(display));
       setDisplay(evaluate(display));
     }
+    
   }
+
+  
+  
+
   return (
     <div className='calcWrap'>
       <div className='textWrap'>
         <h1 className='text'>{display}</h1>
+        {/* <h2>{currentAnswer}</h2> */}
       </div>
       <div className='buttonWrap'>
         {buttonContents.map((char, index) => {
